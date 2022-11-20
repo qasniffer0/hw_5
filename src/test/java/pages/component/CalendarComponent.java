7c5b278f222a94cbb4bb88eac1b5d4231f58d5ac
qasniffer0 <qasniffer0@gmail.com>
2022-11-20 14:22:51 +0500

    Second commit

--- src/test/java/pages/component/CalendarComponent.java
+++ src/test/java/pages/component/CalendarComponent.java
@@ -7,7 +7,7 @@ public class CalendarComponent {
     public void setDate(String day, String month, String year) {
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__ day--017" + day +
+        $(".react-datepicker__ day--0" + day +
                 ":not(.react-datepicker__day--outside-mont)").click();
     }
 }
