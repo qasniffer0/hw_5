7e728f2c3cddd0941bed9383acb2172c67429203
qasniffer0 <qasniffer0@gmail.com>
2022-12-10 22:39:53 +0500

    First commit

--- src/test/java/com/sniffer0/BaseTest.java
+++ src/test/java/com/sniffer0/BaseTest.java
@@ -1,14 +1,40 @@
 package com.sniffer0;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.sniffer0.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 abstract class BaseTest {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.holdBrowserOpen = true;
+       // Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.timeout = 10000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Screen");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
--- src/test/java/com/sniffer0/UserDataPageObjectTest.java
+++ src/test/java/com/sniffer0/UserDataPageObjectTest.java
@@ -3,6 +3,8 @@ package com.sniffer0;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
+import static io.qameta.allure.Allure.step;
+
 public class UserDataPageObjectTest extends BaseTest {
 
     RegistrationPage registrationPage = new RegistrationPage();
@@ -11,31 +13,36 @@ public class UserDataPageObjectTest extends BaseTest {
 
     @Test
     void registrationTest() {
-        registrationPage.openPage();
-        registrationPage.setFirstName(reg.firstName);
-        registrationPage.setLastName(reg.lastName);
-        registrationPage.setUserEmail(reg.userEmail);
-        registrationPage.setGender(reg.userGender);
-        registrationPage.setNumber(reg.userNumber);
-        registrationPage.setBirthDate(reg.day, reg.month, reg.year);
-        registrationPage.setSubjects(reg.subject);
-        registrationPage.chooseHobbie(reg.hobbies);
-        registrationPage.setImgPicture(img);
-        registrationPage.setAddress(reg.address1);
-        registrationPage.scrollToSubmitButton();
-        registrationPage.setStateAndCity(reg.state, reg.city);
-        registrationPage.pressSubmitButton();
-
-        registrationPage.verifyModalAppears()
-                .verifyResult("Student Name", reg.firstName + " " + reg.lastName)
-                .verifyResult("Student Email", reg.userEmail)
-                .verifyResult("Gender", reg.userGender)
-                .verifyResult("Mobile", reg.userNumber)
-                .verifyResult("Date of Birth", reg.day + " " + reg.month + "," + reg.year)
-                .verifyResult("Subjects", reg.subject)
-                .verifyResult("Hobbies", reg.hobbies)
-                .verifyResult("Picture", img)
-                .verifyResult("Address", reg.address1)
-                .verifyResult("State and City", reg.state + " " + reg.city);
+        step("Open registrations form", () -> {
+            registrationPage.openPage();
+        });
+        step("Fill form", () -> {
+            registrationPage.setFirstName(reg.firstName);
+            registrationPage.setLastName(reg.lastName);
+            registrationPage.setUserEmail(reg.userEmail);
+            registrationPage.setGender(reg.userGender);
+            registrationPage.setNumber(reg.userNumber);
+            registrationPage.setBirthDate(reg.day, reg.month, reg.year);
+            registrationPage.setSubjects(reg.subject);
+            registrationPage.chooseHobbie(reg.hobbies);
+            registrationPage.setImgPicture(img);
+            registrationPage.setAddress(reg.address1);
+            registrationPage.scrollToSubmitButton();
+            registrationPage.setStateAndCity(reg.state, reg.city);
+            registrationPage.pressSubmitButton();
+        });
+        step("Check form results", () -> {
+            registrationPage.verifyModalAppears()
+                    .verifyResult("Student Name", reg.firstName + " " + reg.lastName)
+                    .verifyResult("Student Email", reg.userEmail)
+                    .verifyResult("Gender", reg.userGender)
+                    .verifyResult("Mobile", reg.userNumber)
+                    .verifyResult("Date of Birth", reg.day + " " + reg.month + "," + reg.year)
+                    .verifyResult("Subjects", reg.subject)
+                    .verifyResult("Hobbies", reg.hobbies)
+                    .verifyResult("Picture", img)
+                    .verifyResult("Address", reg.address1)
+                    .verifyResult("State and City", reg.state + " " + reg.city);
+        });
     }
 }
